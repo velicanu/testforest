500a75a1769b8353e0eedf17e4434976e8fd833e
Dragos Velicanu <velicanu@mit.edu>
2015-11-20 19:11:26 +0100

    made one script which can run on one or all trees

--- getentries.sh
+++ getentries.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-  echo "Usage: ./getl1rates.sh <rootfile>"
+  echo "Usage: ./getl1rates.sh <rootfile> [justone]"
   exit 1
 fi
 
 echo "{" > getThisEntries.C
-echo "  TFile * _file0 = new TFile(\"/data/velicanu/store/group/phys_heavyions/velicanu/forest/Run2015E/ExpressPhysics/Merged/ExpressHiForest_run262163-262172_100k.root\");" >> getThisEntries.C
+echo "  TFile * _file0 = new TFile(\"$1\");" >> getThisEntries.C
 
 ./lsroot.sh $1 > tmproot.txt
-for i in ` cat tmp | grep TDirectoryFile | sed 's@;1@@g' | awk '{print $3}' ` 
+for i in ` cat tmproot.txt | grep TDirectoryFile | sed 's@;1@@g' | awk '{print $3}' ` 
 do 
-  ./lsdir.sh /data/velicanu/store/group/phys_heavyions/velicanu/forest/Run2015E/ExpressPhysics/Merged/ExpressHiForest_run262163-262172_100k.root $i >  tmpdir.txt
+  ./lsdir.sh $1 $i >  tmpdir.txt
   cat tmpdir.txt | grep TTree |  sed 's@;1@@g' | awk '{print $3}' > tmptree.txt
   for j in `cat tmptree.txt`
   do
     echo "TTree * $i$j = (TTree*) _file0->Get(\"$i/$j\");" >> getThisEntries.C
     echo "std::cout<<\"$i/$j entries = \"<<$i$j->GetEntries()<<std::endl;" >> getThisEntries.C
+    if [[ "${2}" ]]
+    then
+      break
+    fi
   done
+  if [[ "${2}" ]]
+  then
+    break
+  fi
 done
 echo "}" >> getThisEntries.C
 
 root -b -q getThisEntries.C
 
-rm tmpdir.txt tmptree.txt tmproot.txt 
+#rm tmpdir.txt tmptree.txt tmproot.txt 
 
