16012b860a4284e088cf1611ce829ea3b3aa8984
Dragos Velicanu <velicanu@mit.edu>
2015-12-01 19:20:21 +0100

    added fixed getentries

--- getentries.sh
+++ getentries.sh
@@ -5,18 +5,20 @@ then
   exit 1
 fi
 
-echo "{" > getThisEntries.C
-echo "  TFile * _file0 = new TFile(\"$1\");" >> getThisEntries.C
+now=$(date +%s%N)
 
-./lsroot.sh $1 > tmproot.txt
-for i in ` cat tmproot.txt | grep TDirectoryFile | sed 's@;1@@g' | awk '{print $3}' ` 
+echo "{" > ${now}_getThisEntries.C
+echo "  TFile * _file0 = new TFile(\"$1\");" >> ${now}_getThisEntries.C
+
+~/testforest/lsroot.sh $1 > ${now}_tmproot.txt
+for i in ` cat tmproot.txt | grep TDirectoryFile | sed 's@;.*@@g' | awk '{print $3}' ` 
 do 
-  ./lsdir.sh $1 $i >  tmpdir.txt
-  cat tmpdir.txt | grep TTree |  sed 's@;1@@g' | awk '{print $3}' > tmptree.txt
-  for j in `cat tmptree.txt`
+  ~/testforest/lsdir.sh $1 $i >  ${now}_tmpdir.txt
+  cat ${now}_tmpdir.txt | grep TTree |  sed 's@;.*@@g' | awk '{print $3}' > ${now}_tmptree.txt
+  for j in `cat ${now}_tmptree.txt | sort | uniq `
   do
-    echo "TTree * $i$j = (TTree*) _file0->Get(\"$i/$j\");" >> getThisEntries.C
-    echo "std::cout<<\"$i/$j entries = \"<<$i$j->GetEntries()<<std::endl;" >> getThisEntries.C
+    echo "TTree * $i$j = (TTree*) _file0->Get(\"$i/$j\");" >> ${now}_getThisEntries.C
+    echo "std::cout<<\"$i/$j entries = \"<<$i$j->GetEntries()<<std::endl;" >> ${now}_getThisEntries.C
     if [[ "${2}" ]]
     then
       break
@@ -27,9 +29,10 @@ do
     break
   fi
 done
-echo "}" >> getThisEntries.C
+echo "}" >> ${now}_getThisEntries.C
 
-root -b -q getThisEntries.C
+root -b -q ${now}_getThisEntries.C
 
+rm ${now}_*
 #rm tmpdir.txt tmptree.txt tmproot.txt 
 
